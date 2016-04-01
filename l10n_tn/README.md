f49f8e20c34f9da6e9adbfaac6fd386d44175903
Ahmed-Darbi <ahmed.darbi@gmail.com>
2016-04-01 22:39:34 +0200

    Added files via upload

---
+++ l10n_tn/README.md
@@ -0,0 +1,3 @@
+# l10n_tn
+Ceci est le module de base pour gérer la comptabilité pour la Tunisie.
+(odoo)addons module: Comptabilité - Tunisienne By: S.T.industriels: Darbi Ahmed
---
+++ l10n_tn/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name" : "Comptabilité - Tunisienne",
+    "version" : "1.0",
+    "author" : "S.T.industriels: Darbi Ahmed",
+    "website": "http://www.stindustriels.tn/",
+    "category" : "Localization/Account Charts",
+    "description": """
+Ceci est le module de base pour gérer la comptabilité pour la Tunisie .
+=================================================================
+
+Ceci est le module de base pour gérer la comptabilité pour la Tunisie""",
+
+    "depends" : ['base','account'],
+    "init_xml" : [],
+    "data" : [
+        'plan_comptable_general.xml',
+        'tn_pcg_taxes.xml',
+        'tn_tax.xml',
+		'l10n_tn_wizard.xml',
+        'security/ir.model.access.csv',
+		     ],
+    "test": [],
+    "demo_xml" : [],
+    "active": True,
+    "installable": True,
+	'images': ['images/config_chart_l10n_tn.jpg', 'images/l10n_tn_chart.jpg'],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
